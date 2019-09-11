7869b744c170829ae24934e11964665bcec39938
Todor Trundev <ttrundev@visteon.com>
2019-09-11 13:10:30 +0300

    Visualization impromenets
    Draw circle, center point marker, use different line-styles.
    The axis/figure objects are passed as arguments to avoid use of the
    pyplot current ones.

--- circle_center.py
+++ circle_center.py
@@ -7,10 +7,13 @@ Tested on python 3.7.2. Reuires: numpy, matplotlib
 import numpy
 
 import matplotlib.pyplot as pyplot
+import matplotlib.patches as patches
 
-CHORD_COLOR = 'green'
-BISECT_COLOR = 'yellow'
-RADIUS_COLOR = 'blue'
+CHORD_FMT = '.--g'
+BISECT_FMT = 'y'
+RADIUS_FMT = 'b'
+CIRCLE_STYLE = {'alpha':.5, 'fill':False, 'linestyle':':', 'color':'blue'}
+POINT_FMT = '+r'
 Z_VECTOR = numpy.array([0,0,1])
 
 #
@@ -37,30 +40,31 @@ def intersect(vect, point0, point1):
 #
 # pyplot helpers
 #
-def plot_line(start, end, color):
-    return pyplot.plot([start[0], end[0]], [start[1], end[1]], color=color)
+def plot_line(ax, start, end,  *args, **kwargs):
+    return ax.plot([start[0], end[0]], [start[1], end[1]],  *args, **kwargs)
 
-def plot_vector(start, vect, color):
-    return plot_line(start, vect + start, color=color)
+def plot_vector(ax, start, vect,  *args, **kwargs):
+    return plot_line(ax, start, vect + start,  *args, **kwargs)
 
-def do_ginput():
+def do_ginput(fig):
     pyplot.title('Enter 3 circle points')
-    input = pyplot.ginput(3)
+    input = fig.ginput(3)
+    pyplot.title('Circle')
     print('Input point', input)
     input = numpy.array(input)
 
-    # Display input vectors
-    pyplot.plot(input[:,0], input[:,1], color=CHORD_COLOR, label='Chords')
+    # Draw input vectors
+    fig.gca().plot(input[:,0], input[:,1], CHORD_FMT, label='Chords')
     return input
 
-def get_bisect_lines(vectors):
+def get_bisect_lines(vectors, ax):
     # Bisect-lines: Perpendicular vectors starting at the middle of input chords
     bisect_lines = []
     for idx in range(len(vectors) - 1):
         chord = vectors[idx + 1] - vectors[idx]
         perp = perpendicular(chord)
         start = chord * .5 + vectors[idx]
-        line, = plot_vector(start, perp, BISECT_COLOR)
+        line, = plot_vector(ax, start, perp, BISECT_FMT)
         bisect_lines.append({'start': start, 'end': perp + start})
 
     if line:
@@ -71,11 +75,13 @@ def get_bisect_lines(vectors):
 # main
 #
 def main():
-    pyplot.gca().set_autoscale_on(False)
-    pyplot.gca().set_aspect('equal')
+    fig = pyplot.gcf()
+    ax = fig.gca()
+    ax.set_autoscale_on(False)
+    ax.set_aspect('equal')
 
-    vectors = do_ginput()
-    bisect_lines = get_bisect_lines(vectors)
+    vectors = do_ginput(fig)
+    bisect_lines = get_bisect_lines(vectors, ax)
 
     # Intersection of bisect lines
     start = bisect_lines[1]['start']
@@ -83,9 +89,13 @@ def main():
     center = vect + start
     print('Center', center)
 
+    # Draw center and the circle
+    ax.plot(*center[:2], POINT_FMT)
+    ax.add_artist(patches.Circle(center[:2], vect_len(vectors[0] - center), **CIRCLE_STYLE))
+
     for vect in vectors:
         print('\tRadius len', vect_len(vect - center))
-        line, = plot_line(vect, center, RADIUS_COLOR)
+        line, = plot_line(ax, vect, center, RADIUS_FMT)
     if line:
         line.set_label('Radiuses')
 
