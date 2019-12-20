c5e244b28842d8aadc380c5d16e2fcc4bdfde2bf
Todor Trundev <ttrundev@visteon.com>
2019-12-20 15:30:24 +0200

    Initial version with generic transformation
    Add X/Y/Z sliders and source/target, translate/rotate/scale radio
    buttons. The sliders transformate the source or target around the
    specific axis.

--- emi.py
+++ emi.py
@@ -46,18 +46,24 @@ TARGET_POINTS = [
 #    num=TGT_ROW_STEPS
 #).reshape((-1,3))
 
-# Slider origin/direction parameters
-SOURCE_SLIDER_ORG = [0.5, 1, 0]
-TARGET_SLIDER_DIR = [0, 1, 0]
+# Transformation parameters
+TRANSFORM_ORG = [0.5, 1, 0]
 
 FIELD_SCALE = .2
 EMF_SCALE = .1
 
 AX_MARGIN = .02
-AX_BTN_HEIGHT = .2
+AX_BTN_HEIGHT = .3
 AX_BTN_WIDTH = .25
-AX_TEXT_WIDTH = .08
-AX_NUM_SLIDERS = 2
+AX_TEXT_WIDTH = .05
+AX_NUM_SLIDERS = 3
+
+LABEL_TRANSLATE = 'Translate'
+LABEL_ROTATE = 'Rotate'
+LABEL_SCALE = 'Scale'
+TRANSLATE_PARAMS = [-2, 2, 0]
+ROTATE_PARAMS = [-360, 360, 0]
+SCALE_PARAMS = [0, 2, 1]
 
 SOURCE_FMT = dict(color='green', label='Source')
 TARGET_FMT = dict(color='blue', marker='+', label='Target')
@@ -110,6 +116,19 @@ def deflate_rect(rect, hor_margin=AX_MARGIN, vert_margin=AX_MARGIN):
     rect[2] -= 2 * hor_margin
     rect[3] -= 2 * vert_margin
     return rect
+
+def rotate_point(pt, axis, pos):
+    # Shift axes
+    pt = numpy.array([*pt[axis + 1:], *pt][:pt.size])
+
+    # Perform 2D rotation on (pt[0], pt[1])
+    x = pt[0] * numpy.cos(pos) - pt[1] * numpy.sin(pos)
+    y = pt[0] * numpy.sin(pos) + pt[1] * numpy.cos(pos)
+    pt[0], pt[1] = x, y
+
+    # Shift-back axes
+    pt = numpy.array([*pt[pt.size - axis - 1:], *pt][:pt.size])
+    return pt
 
 class main_data:
     src_lines = None
@@ -166,7 +185,7 @@ class main_data:
     def get_collections(self):
         return [self.src_coll, self.tgt_coll, self.B_coll, self.dB_coll, self.emf_coll]
 
-class on_clicked:
+class visible_clicked:
     def __init__(self, data):
         self.data = data
 
@@ -177,30 +196,101 @@ class on_clicked:
                 break
         pyplot.draw()
 
-class src_changed:
+class transform:
+    xform_src = None    # True: source, False: target
+    xform_type = None   # LABEL_TRANSLATE, LABEL_ROTATE, LABEL_SCALE
+    sliders = [None, None, None]
+
     def __init__(self, data, origin):
         self.data = data
         self.origin = origin
 
+    def set_slider(self, idx, slider):
+        self.sliders[idx] = slider
+
+    def src_type_changed(self):
+        active = not(self.xform_src is None or self.xform_type is None)
+        slider_params = None
+        if active:
+            if self.xform_type == LABEL_TRANSLATE:
+                slider_params = TRANSLATE_PARAMS
+            elif self.xform_type == LABEL_ROTATE:
+                slider_params = ROTATE_PARAMS
+            elif self.xform_type == LABEL_SCALE:
+                slider_params = SCALE_PARAMS
+        for s in self.sliders:
+            if s is not None:
+                s.set_active(active)
+                s.reset()
+                #TODO: Change slider range
+                #if slider_params:
+                #
+                #    s.valmin, s.valval = slider_params[:2]
+                #    s.set_val(slider_params[2])
+
+    def src_changed(self, src):
+        self.xform_src = src
+        self.src_type_changed()
+
+    def type_changed(self, xform_type):
+        self.xform_type = xform_type
+        self.src_type_changed()
+
+    def pos_changed(self, slider_idx, pos):
+        if self.xform_src is True:
+            org_pts = numpy.array(SOURCE_POLYLINE)
+            pts = self.data.src_lines
+        elif self.xform_src is False:
+            org_pts = numpy.array(TARGET_POINTS)
+            pts = self.data.tgt_pts
+        else:
+            return
+
+        origin = self.origin[slider_idx]
+        if self.xform_type == LABEL_TRANSLATE:
+            for idx, pt in enumerate(pts):
+                pt[slider_idx] = org_pts[idx][slider_idx] + pos
+        elif self.xform_type == LABEL_ROTATE:
+            for idx, pt in enumerate(pts):
+                pts[idx] = rotate_point(org_pts[idx] - self.origin, slider_idx, pos) + self.origin
+        elif self.xform_type == LABEL_SCALE:
+            for idx, pt in enumerate(pts):
+                pt[slider_idx] = (org_pts[idx][slider_idx] - origin) * pos + origin
+        else:
+            return
+
+        if self.xform_src == True:
+            self.data.redraw(pts, None)
+        elif self.xform_src == False:
+            self.data.redraw(None, pts)
+        return
+
+class transform_what:
+    def __init__(self, xform):
+        self.xform = xform
+
+    def __call__(self, label):
+        if label == self.xform.data.src_coll.get_label():
+            self.xform.src_changed(True)
+        elif label == self.xform.data.tgt_coll.get_label():
+            self.xform.src_changed(False)
+        else:
+            self.xform.src_changed(None)
+
+class transform_type:
+    def __init__(self, xform):
+        self.xform = xform
+
+    def __call__(self, label):
+        self.xform.type_changed(label)
+
+class xform_changed:
+    def __init__(self, xform, idx):
+        self.xform = xform
+        self.idx = idx
+
     def __call__(self, pos):
-        src_lines = numpy.array(SOURCE_POLYLINE)
-        for idx, pt in enumerate(src_lines):
-            src_lines[idx] = (pt - self.origin) * pos + self.origin
-
-        self.data.redraw(src_lines, None)
-        pyplot.draw()
-
-class tgt_changed:
-    def __init__(self, data, move_dir):
-        self.data = data
-        self.move_dir = move_dir
-
-    def __call__(self, pos):
-        tgt_pts = numpy.array(TARGET_POINTS)
-        for idx, _ in enumerate(tgt_pts):
-            tgt_pts[idx] += self.move_dir * pos
-
-        self.data.redraw(None, tgt_pts)
+        self.xform.pos_changed(self.idx, pos)
         pyplot.draw()
 
 def main(argv):
@@ -222,21 +312,32 @@ def main(argv):
     labels = [coll.get_label() for coll in colls]
     visibility = [coll.get_visible() for coll in colls]
     check = widgets.CheckButtons(rax, labels, visibility)
-    check.on_clicked(on_clicked(data))
+    check.on_clicked(visible_clicked(data))
 
-    # Slider to scale source lines (slider 1)
-    rect = [AX_BTN_WIDTH, 1 * AX_BTN_HEIGHT / AX_NUM_SLIDERS,
-            1 - AX_BTN_WIDTH, AX_BTN_HEIGHT / AX_NUM_SLIDERS]
-    rax = pyplot.axes(deflate_rect(rect, AX_TEXT_WIDTH + AX_MARGIN))
-    src_slider = widgets.Slider(rax, data.src_coll.get_label(), 0, 2, 1)
-    src_slider.on_changed(src_changed(data, numpy.array(SOURCE_SLIDER_ORG)))
+    # Transformation selection context
+    xform = transform(data, numpy.array(TRANSFORM_ORG))
 
-    # Slider to move target points (slider 2)
-    rect = [AX_BTN_WIDTH, 0 * AX_BTN_HEIGHT / AX_NUM_SLIDERS,
-            1 - AX_BTN_WIDTH, AX_BTN_HEIGHT / AX_NUM_SLIDERS]
-    rax = pyplot.axes(deflate_rect(rect, AX_TEXT_WIDTH + AX_MARGIN))
-    tgt_slider = widgets.Slider(rax, data.tgt_coll.get_label(), -2, 2, 0)
-    tgt_slider.on_changed(tgt_changed(data, numpy.array(TARGET_SLIDER_DIR)))
+    # Radio buttons to select what to control
+    rect = [AX_BTN_WIDTH, 5 * AX_BTN_HEIGHT / 9, AX_BTN_WIDTH, 4 * AX_BTN_HEIGHT / 9]
+    rax = fig.add_axes(deflate_rect(rect))
+    radio = widgets.RadioButtons(rax, [data.src_coll.get_label(), data.tgt_coll.get_label()], None)
+    radio.on_clicked(transform_what(xform))
+
+    # Radio buttons to select how to control it
+    rect = [AX_BTN_WIDTH, 0, AX_BTN_WIDTH, 5 * AX_BTN_HEIGHT / 9]
+    rax = fig.add_axes(deflate_rect(rect))
+    radio2 = widgets.RadioButtons(rax, [LABEL_TRANSLATE, LABEL_ROTATE, LABEL_SCALE], None)
+    radio2.on_clicked(transform_type(xform))
+
+    # Sliders to transform X/Y/Z
+    for idx in range(AX_NUM_SLIDERS):
+        rect = [2 * AX_BTN_WIDTH, (AX_NUM_SLIDERS - 1 - idx) * AX_BTN_HEIGHT / AX_NUM_SLIDERS,
+                1 - 2 * AX_BTN_WIDTH, AX_BTN_HEIGHT / AX_NUM_SLIDERS]
+        rax = pyplot.axes(deflate_rect(rect, AX_TEXT_WIDTH + AX_MARGIN))
+        name = bytearray(['X'.encode()[0] + idx]).decode()
+        slider = widgets.Slider(rax, name, -2, 2, 0)
+        slider.on_changed(xform_changed(xform, idx))
+        xform.set_slider(idx, slider)
 
 
     ax.legend()
