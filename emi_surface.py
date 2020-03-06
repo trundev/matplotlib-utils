ac4b359ba5b676ef624d92c4329e0c8db969505d
Todor Trundev <ttrundev@visteon.com>
2020-03-06 12:24:27 +0200

    Ensure stepping is perpendicular to gradB, etc.
    The step along the "u" surface coordinate starts from vector, that is
    perpendicular to "gradB". This is to handle the approximation issues in
    the controversial case, when "B" and "gradB" are NOT perpendicular.
    
    Various code cleanups.

--- emi_surface.py
+++ emi_surface.py
@@ -10,6 +10,21 @@ import emi_calc
 U_MAX = 10
 V_MAX = 10
 MAX_ITERATIONS = 10
+
+#
+# Source current flow
+#
+SRC_Z_STEP = 0  # 0.01
+SOURCE_POLYLINE = [
+   [0., 0., 0 * SRC_Z_STEP],
+   [1., 0., 1 * SRC_Z_STEP],    # right
+#   [1., 1., 2 * SRC_Z_STEP],    # up
+#   [0., 1., 3 * SRC_Z_STEP],    # left
+#   [0., 0., 4 * SRC_Z_STEP],    # down
+]
+
+BASE_POINT = [0.5, -2., 1.]
+
 #
 # From emi.py
 #
@@ -27,6 +42,12 @@ def plot_source(ax, src_lines):
 gradB_FMT['linestyle']='-'
 FAIL_FMT = dict(color='red', marker='o', label='Failure')
 
+#
+# Equipotential surface approximation
+#
+def step_along(pt, vect, center, coef):
+    center_v = pt - center
+    return pt + vect / numpy.sqrt(vect.dot(vect) / center_v.dot(center_v)) * coef
 
 def equipotential_surface(base_pt, src_lines, tolerance=.01):
     base_params = emi_calc.calc_all_emis(base_pt, src_lines)
@@ -57,6 +78,7 @@ def equipotential_surface(base_pt, src_lines, tolerance=.01):
             for iter in range(MAX_ITERATIONS):
                 emi_params = emi_calc.calc_all_emis(pt, src_lines)
                 B = emi_params['B']
+                gradB = emi_params['gradB']
                 B_len = numpy.sqrt(B.dot(B))
                 dB = base_B_len - B_len
                 if abs(dB) < tolerance * base_B_len:
@@ -65,7 +87,6 @@ def equipotential_surface(base_pt, src_lines, tolerance=.01):
                     break
 
                 # Location correction: "dB / |gradB|" along the normalized "gradB"
-                gradB = emi_params['gradB']
                 pt += gradB * dB / gradB.dot(gradB)
                 print('  pt', pt, 'dB', dB, '(%.1f%%)'%(dB / base_B_len * 100))
 
@@ -81,36 +102,18 @@ def equipotential_surface(base_pt, src_lines, tolerance=.01):
             if base_params is None:
                 base_params = emi_params
 
-            # Do step along "u"
-            center_dist = pt - center_pt
-            center_dist = numpy.sqrt(center_dist.dot(center_dist))
-            pt += B / B_len * center_dist * tan_phi
+            # Do step along "u", moving toward "B", but perpendicular to "gradB"
+            # (this is the vector "gradB x B x gradB")
+            pt = step_along(pt, numpy.cross(numpy.cross(gradB, B), gradB), center_pt, tan_phi)
             u += 1
 
-        # Do step along "v"
+        # Do step along "v", moving perpendicular to "u"
         pt = base_params['pt']
-        center_dist = pt - center_pt
-        center_dist = numpy.sqrt(center_dist.dot(center_dist))
-        step_v = numpy.cross(base_params['gradB'], base_params['B'])
-        pt += step_v / numpy.sqrt(step_v.dot(step_v)) * center_dist * tan_psi
+        pt = step_along(pt, numpy.cross(base_params['gradB'], base_params['B']), center_pt, tan_psi)
         v += 1
 
     print('Max iterations', max_iter, 'at', max_iter_u_v, ',', failed_pts, 'failed')
     return surface
-
-#
-# Source current flow
-#
-SRC_Z_STEP = 0  # 0.01
-SOURCE_POLYLINE = [
-   [0., 0., 0 * SRC_Z_STEP],
-   [1., 0., 1 * SRC_Z_STEP],    # right
-#   [1., 1., 2 * SRC_Z_STEP],    # up
-#   [0., 1., 3 * SRC_Z_STEP],    # left
-#   [0., 0., 4 * SRC_Z_STEP],    # down
-]
-
-BASE_POINT = [0.5, -2., 1.]
 
 #
 # Utilities
@@ -131,6 +134,54 @@ def strip_nans(a):
     return a, nans
 
 #
+# Data validation
+#
+def check_result(src_lines, surface):
+    pts = surface['pt']
+    B_vecs = surface['B']
+    gradB_vecs = surface['gradB']
+    ret = True
+
+    print('Re-check equipotential surface:', B_vecs.shape[:-1])
+    print('  B vectors')
+    B_vec_lens = vect_lens(B_vecs)
+    B_vec_lens, nans = strip_nans(B_vec_lens)
+    print('    Lengths (mean/max/min/std):', B_vec_lens.mean(), B_vec_lens.max(), B_vec_lens.min(), B_vec_lens.std(), ',', nans, 'nans')
+    if B_vec_lens.std() > 1e-3:
+        print('Warning: Standard deviation is too big:', B_vec_lens.std(), file=sys.stderr)
+
+    print('  Recalculate B vectors')
+    params = emi_calc.calc_all_emis(pts, src_lines)
+    B_recheck = params['B']
+    B_dif = B_recheck - B_vecs
+    if B_dif.min() != 0 or B_dif.max() != 0:
+        print('Warning: B_recheck - B_vec (max/min):', B_dif.max(), B_dif.min(), file=sys.stderr)
+        ret = False
+    B_recheck_lens = vect_lens(B_recheck)
+    B_recheck_lens, nans = strip_nans(B_recheck_lens)
+    print('    Lengths (mean/max/min):', B_recheck_lens.mean(), B_recheck_lens.max(), B_recheck_lens.min(), ',', nans, 'nans')
+
+    # Angle between B and grad-B (perpendicular when the source is a line)
+    print('  B perpendicular to gradB (B . gradB ~ 0)')
+    B_dot_gradB = vect_dot(B_vecs, gradB_vecs)
+    B_dot_gradB, nans = strip_nans(B_dot_gradB)
+    print('    B . gradB (max/min):', B_dot_gradB.max(), B_dot_gradB.min(), ',', nans, 'nans')
+    # Angle between (B . gradB) / (|B| * |gradB|)
+    gradB_vec_lens = vect_lens(gradB_vecs)
+    gradB_vec_lens, _ = strip_nans(gradB_vec_lens)
+    if B_dot_gradB.size == gradB_vec_lens.size and gradB_vec_lens.size == B_vec_lens.size:
+        B_gradB_angle = B_dot_gradB / (B_vec_lens * gradB_vec_lens)
+        B_gradB_angle = numpy.arccos(B_gradB_angle)
+        B_gradB_angle *= 180 / numpy.pi
+        print('    Angle between B and gradB (max/min deg):', B_gradB_angle.max(), B_gradB_angle.min())
+    else:
+        print('Warning: Different number of non-nan values in "B . gradB", "gradB" and "B":',
+                B_dot_gradB.size, gradB_vec_lens.size, B_vec_lens.size,
+                file=sys.stderr )
+        ret = False
+    return ret
+
+#
 # Plot
 #
 def main(base_pt, src_lines):
@@ -139,40 +190,18 @@ def main(base_pt, src_lines):
     ax = fig.gca(projection='3d')
 
     surface = equipotential_surface(base_pt, src_lines)
-    pts = surface['pt']
-    B_vecs = surface['B']
-    gradB_vecs = surface['gradB']
 
     # Re-check results
     if True:
-        print('Re-check equipotential surface:', B_vecs.shape[:-1])
-        print('  B vectors')
-        B_vec_lens = vect_lens(B_vecs)
-        B_vec_lens, nans = strip_nans(B_vec_lens)
-        print('    Lengths (mean/max/min):', B_vec_lens.mean(), B_vec_lens.max(), B_vec_lens.min(), ',', nans, 'nans')
-
-        print('  Recalculate B vectors')
-        params = emi_calc.calc_all_emis(pts, src_lines)
-        B_recheck = params['B']
-        B_dif = B_recheck - B_vecs
-        if B_dif.min() != 0 or B_dif.max() != 0:
-            print('Warning: B_recheck - B_vec (max/min):', B_dif.max(), B_dif.min(), file=sys.stderr)
-        B_recheck_lens = vect_lens(B_recheck)
-        B_recheck_lens, nans = strip_nans(B_recheck_lens)
-        print('    Lengths (mean/max/min):', B_recheck_lens.mean(), B_recheck_lens.max(), B_recheck_lens.min(), ',', nans, 'nans')
-
-        # Angle between B and grad-B (perpendicular when the source is a line)
-        print('  B perpendicular to gradB (B . gradB ~ 0)')
-        B_dot_gradB = vect_dot(B_vecs, gradB_vecs)
-        print('    B . gradB (max/min):', B_dot_gradB.max(), B_dot_gradB.min())
-        # Angle between (B . gradB) / (|B| * |gradB|)
-        B_gradB_angle = B_dot_gradB / (vect_lens(B_vecs) * vect_lens(gradB_vecs))
-        B_gradB_angle *= 180 / numpy.pi
-        print('    Angle between B and gradB (max/min deg):', B_gradB_angle.max(), B_gradB_angle.min())
+        check_result(src_lines, surface)
 
     # Source line and base target point
     plot_source(ax, src_lines)
     ax.scatter(*base_pt, **TARGET_FMT)
+
+    pts = surface['pt']
+    B_vecs = surface['B']
+    gradB_vecs = surface['gradB']
 
     # Identify failed points
     pts_fails = numpy.full_like(pts, numpy.nan)
